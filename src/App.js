846b230fe34b05b3db1f48e91371035d8a3bb948
nikhilpant00 <105990262+nikhilpant00@users.noreply.github.com>
2023-04-05 18:10:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import { Parallax, ParallaxLayer } from '@react-spring/parallax'
-import TextBlock from './textBlock';
-import './App.css';
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import TextBlock from "./textBlock";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <Parallax pages={2} style={{ top: '0', left: '0' }} class="animation">
-        <ParallaxLayer offset={0} speed={0.25}>
-          <div class="animation_layer parallax" id="artback"></div>
-        </ParallaxLayer>
+      <Parallax
+        pages={2}
+        style={{ top: "0", left: "0", backgroundColor: "#e7f3ff" }}
+        class="animation"
+      >
+        <div className="test">
+          <ParallaxLayer offset={0} horizontal speed={3}>
+            <div class="animation_layer parallax " id="artbackL"></div>
+          </ParallaxLayer>
+        </div>
+        <div className="test2">
+          <ParallaxLayer offset={0} horizontal speed={-3}>
+            <div class="animation_layer parallax " id="artbackR"></div>
+          </ParallaxLayer>
+        </div>
         <ParallaxLayer offset={0} speed={0.3}>
           <div class="animation_layer parallax" id="mountain"></div>
         </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={-0.1}>
+        {/* <ParallaxLayer offset={0} speed={-0.5}>
           <div class="animation_layer parallax" id="logoland"></div>
-        </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={0.3}>
+        </ParallaxLayer> */}
+        <ParallaxLayer offset={0} speed={0.8}>
           <div class="animation_layer parallax" id="jungle1"></div>
         </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={0.35}>
+        {/* <ParallaxLayer offset={0} speed={0.35}>
           <div class="animation_layer parallax" id="jungle2"></div>
         </ParallaxLayer>
         <ParallaxLayer offset={0} speed={0.5}>
@@ -26,17 +37,21 @@ function App() {
         </ParallaxLayer>
         <ParallaxLayer offset={0} speed={0.45}>
           <div class="animation_layer parallax" id="jungle4"></div>
-        </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={0.40}>
-          <div class="animation_layer parallax" id="manonmountain"></div>
-        </ParallaxLayer>
-        <ParallaxLayer offset={0} speed={0.35}>
+        </ParallaxLayer> */}
+        <ParallaxLayer offset={0} speed={1}>
           <div class="animation_layer parallax" id="jungle5"></div>
         </ParallaxLayer>
-        <ParallaxLayer offset={1} speed={0.25}>
+        {/* <ParallaxLayer offset={0} speed={0.4}>
+          <div class="animation_layer parallax" id="manonmountain"></div>
+        </ParallaxLayer> */}
+        {/* <ParallaxLayer offset={1} speed={0.25}>
           <TextBlock />
-        </ParallaxLayer>
+        </ParallaxLayer> */}
+        {/* <ParallaxLayer offset={.9} factor={.9} speed={1}>
+          <TextBlock />
+        </ParallaxLayer> */}
       </Parallax>
+      {/* <div style={{height:'10rem' ,background:'red', textAlign:'center'}}>hello world</div> */}
     </div>
   );
 }
